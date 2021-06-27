2513fba62c4561400a11d4c8e949cf93644cac62
张亚飞 <672233580@qq.com>
2021-06-27 20:37:06 +0800

    Initial Commit

---
+++ ScrollToTopUI/ScrollToTopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToTopUIApp.swift
+//  ScrollToTopUI
+//
+//  Created by 张亚飞 on 2021/6/27.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToTopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
